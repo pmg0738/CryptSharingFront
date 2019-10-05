3921dc82502c66c6d169df10779883d0f058ce35
moriarty-prof <hyt.cal1023@gmail.com>
2019-10-05 19:45:14 +0900

    merge develop-park

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import './App.css';
 
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+
+import {
+  Col,
+  Row,  
+} from 'react-bootstrap';
+
+import MainSearch from './screens/MainSearch';
 import ItemList from './screens/ItemList';
+import ItemDetail from './screens/ItemDetail';
+import ChatScreen from './screens/ChatScreen';
 
-function App() {
+
+export default class App extends React.Component{
+  render(){
   return (
-    <div className="App">
-      <ItemList/>
-    </div>
-  );
+      <div className="App">
+        <BrowserRouter>
+          <Row>
+            <Col></Col>
+            <Col>
+              <Route path='/MainSearch' component={MainSearch}/>
+              <Route path='/ItemList' component={ItemList}/>
+              <Route path='/ItemDetail' component={ItemDetail}/>
+              <Route path='/ChatScreen' component={ChatScreen}/>
+            </Col>
+          </Row>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
-export default App;
+
---
+++ src/screens/MainSearch/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class MainSearch extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
