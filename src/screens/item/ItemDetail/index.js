982a7582347f7258d2efa181b2f96c80f1c3abcc
pmg0738 <pmg0738@naver.com>
2019-12-06 07:37:31 +0900

    changed a lot item detail UI feeTable and PostUser etc...

--- src/screens/item/ItemDetail/index.js
+++ src/screens/item/ItemDetail/index.js
@@ -3,7 +3,6 @@ import './style.scss';
 import { connect } from 'react-redux';
 import { fetchClickedItem } from '../../../redux/actions';
 import { 
-	Button,
 	Image,
 } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
@@ -20,10 +19,16 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import Avatar from '@material-ui/core/Avatar';
-import { green } from '@material-ui/core/colors';
+import { green, red } from '@material-ui/core/colors';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import TextField from '@material-ui/core/TextField';
+import InsertEmoticonRoundedIcon from '@material-ui/icons/InsertEmoticonRounded';
+import SentimentVeryDissatisfiedRoundedIcon from '@material-ui/icons/SentimentVeryDissatisfiedRounded';
 
 import cat from '../../../images/cup.jpg';
-import { flexbox } from '@material-ui/system';
+
 
 
 class ItemDetail extends React.Component {
@@ -172,41 +177,82 @@ class ItemDetail extends React.Component {
 }
 
 renderImage = () => {
-	console.log('BEFORE if', this.props.item.images);
     if(this.props.item.images) {
-        return <Image className="" src={this.props.item.images[0].url} />
+		return <Image style={{width:"450px", height:"450px", marginBottom:"10px"}} src={this.props.item.images[0].url} />
 	}
-	console.log('AFTER if', this.props.item.images);
     return <div/>
 }
   
+showEmoticon = (itemStatus) =>{
+	if(itemStatus){			
+		return <InsertEmoticonRoundedIcon style={{color:green[500], width:"40px", height:"40px"}}/>
+	}else{
+		return(
+				<div>
+					<SentimentVeryDissatisfiedRoundedIcon style={{color:red[500], width:"40px", height:"40px"}}/>
+					<div style={{fontSize:"10px", opacity:"0.7"}}>12/25から可能</div>
+				</div>
+			)
+	}
+}
+
 renderFeeTable = () => {	
 	return (
-<div style={{backgroundColor:"green"}}>
-			  <Table stickyHeader aria-label="sticky table" style={{width:"350px"}}>
-				<TableHead>
-					<TableCell key='hour_fee' align='center' style={{fontSize:"17px"}}>1時間</TableCell>
-					<TableCell key='day_fee' align='center' style={{fontSize:"17px"}}>1日</TableCell>
-					<TableCell key='week_fee' align='center' style={{fontSize:"17px"}}>1週間</TableCell>
-					<TableCell key='assure_fee' align='center' style={{fontSize:"17px"}}>担保</TableCell>
-				</TableHead>
-				<TableBody>
-					<TableCell align='center' style={{color:"white", fontSize:"15px"}}>￥{this.props.item.fee_per_hour}</TableCell>
-					<TableCell align='center' style={{color:"white", fontSize:"15px"}}>￥{this.props.item.fee_per_day}</TableCell>
-					<TableCell align='center' style={{color:"white", fontSize:"15px"}}>￥500</TableCell>
-					<TableCell align='center' style={{color:"white", fontSize:"15px"}}>￥2000</TableCell>
-				</TableBody>
-			  </Table>
-</div>
+			<Table stickyHeader aria-label="sticky table" style={{width:"450px", marginBottom:"10px"}}>
+			<TableHead>
+				<TableCell key='hour_fee' align='center' style={{fontSize:"17px", fontWeight:"900"}}>1時間</TableCell>
+				<TableCell key='day_fee' align='center' style={{fontSize:"17px", fontWeight:"900"}}>1日</TableCell>
+				<TableCell key='week_fee' align='center' style={{fontSize:"17px", fontWeight:"900"}}>1週間</TableCell>
+				<TableCell key='assure_fee' align='center' style={{fontSize:"17px", fontWeight:"900"}}>担保</TableCell>
+				<TableCell key='assure_fee' align='center' style={{fontSize:"17px", fontWeight:"900"}}>貸し出し</TableCell>
+			</TableHead>
+			<TableBody>
+				<TableCell align='center' style={{color:"white", fontSize:"15px", fontWeight:"900"}}>￥{this.props.item.fee_per_hour}</TableCell>
+				<TableCell align='center' style={{color:"white", fontSize:"15px", fontWeight:"900"}}>￥{this.props.item.fee_per_day}</TableCell>
+				<TableCell align='center' style={{color:"white", fontSize:"15px", fontWeight:"900"}}>￥500</TableCell>
+				<TableCell align='center' style={{color:"white", fontSize:"15px", fontWeight:"900"}}>￥2000</TableCell>
+				<TableCell align='center' style={{color:"white", fontSize:"15px", fontWeight:"900"}}>
+					{this.showEmoticon(0)}
+					{/* <InsertEmoticonRoundedIcon style={{color:green[500], width:"40px", height:"40px"}}/> */}
+				</TableCell>
+			</TableBody>
+			</Table>
 	);
 }
+
+renderStar = (valueOfPostUser) =>{
+								
+	let FullStar = <StarIcon style={{color:"yellow", marginTop:"10px"}}/>;
+	let HalfStar = <StarHalfIcon style={{color:"yellow", marginTop:"10px"}}/>;
+	let EmptyStar = <StarBorderIcon style={{color:"yellow", marginTop:"10px"}}/>;
+	let StarArray = [];
+	switch(valueOfPostUser){
+		case 5:
+			StarArray = [FullStar,FullStar,FullStar,FullStar,FullStar];
+			return StarArray;
+		case 4:
+			StarArray = [FullStar,FullStar,FullStar,FullStar,EmptyStar];
+			return StarArray;
+		case 3:
+			StarArray = [FullStar,FullStar,FullStar,EmptyStar,EmptyStar];
+			return StarArray;
+		case 2:
+			StarArray = [FullStar,FullStar,EmptyStar,EmptyStar,EmptyStar];
+			return StarArray;
+		case 1:
+			StarArray = [FullStar,EmptyStar,EmptyStar,EmptyStar,EmptyStar];
+			return StarArray;
+	}
+		
+}
+
 	render() {
 		return (
 				<div>
 					<Grid container>
-						<Grid sm={12} md={6} style={{backgroundColor:"yellow"}}>
+					<Button variant="contained" color="primary" style={{position:"fixed", right:"50px"}}>一覧に戻る</Button>
+						<Grid  sm={12} md={6} container direction="column" justify="center" alignItems="center" style={{}}>
 							{this.renderImage()}
-							{/* <img className="item-detail-selected-pic" src={this.props.item.}/> */}
 							<Grid>
 								<ArrowBackIosIcon style={{color: green[500]}}/>
 								{this.threeArray(this.state.images, this.state.imageStartIndex).map(image => 
@@ -219,28 +265,30 @@ renderFeeTable = () => {
 								<ArrowForwardIosIcon style={{color: green[500]}}/>
 							</Grid>
 						</Grid>
-						<Grid sm={12} md={6} style={{backgroundColor:"skyblue"}}>
-							<Avatar src={cat} style={{width:"45px", height:"45px"}}/>
-							<div style={{fontSize:"20px", color:"white", margin:"7px", backgroundColor:"purple"}}>upallnight0738</div>
-							<StarIcon style={{color:"yellow", backgroundColor:"purple", marginTop:"7px"}}/>
-							<StarIcon style={{color:"yellow", backgroundColor:"purple", marginTop:"7px"}}/>
-							<StarIcon style={{color:"yellow", backgroundColor:"purple", marginTop:"7px"}}/>
-							<StarHalfIcon style={{color:"yellow", backgroundColor:"purple", marginTop:"7px"}}/>
-							<StarBorderIcon style={{color:"yellow", backgroundColor:"purple", marginTop:"7px"}}/>
-						</Grid>
-						{/* <div>
-							<Grid xs={12} style={{backgroundColor:"skyblue", height:"50px"}}>
+						<Grid sm={12} md={6} style={{backgroundColor:""}}>
+							<Grid container direction="row" justify="flex-start" style={{marginBottom:"20px"}}>
 								<Avatar src={cat} style={{width:"45px", height:"45px"}}/>
-								<div style={{fontSize:"20px", color:"white", margin:"7px", backgroundColor:"purple"}}>upallnight0738</div>
-								<StarIcon style={{color:"yellow", backgroundColor:"purple", marginTop:"7px"}}/>
-								<StarIcon style={{color:"yellow", backgroundColor:"purple", marginTop:"7px"}}/>
-								<StarIcon style={{color:"yellow", backgroundColor:"purple", marginTop:"7px"}}/>
-								<StarHalfIcon style={{color:"yellow", backgroundColor:"purple", marginTop:"7px"}}/>
-								<StarBorderIcon style={{color:"yellow", backgroundColor:"purple", marginTop:"7px"}}/>
+								<div style={{fontSize:"20px", color:"white", margin:"7px"}}>upallnight0738</div>
+								{this.renderStar(3)}
 							</Grid>
-							<div style={{color:"white"}}>料金</div>
+							<div style={{color:"white", fontSize:"20px", fontWeight:"900", marginBottom:"10px"}}>料金</div>
 							{this.renderFeeTable()}
-						</div> */}
+							
+							<TextField
+								id="outlined-multiline-static"
+								label={"Air Jordan\nサイズ：29\n購入時価格：21600円"}
+								disabled
+								multiline
+								rows="10"
+								className="item-post-else-info"
+								placeholder=''
+								margin="normal"
+								variant="outlined"
+								InputProps={{reaOnly:true}}
+								style={{backgroundColor:"white", width:"500px"}}
+							/>
+							<Button variant="contained" color="primary" style={{width:"500px", height:"80px", fontSize:"30px", fontWeight:"900"}}>リクエスト画面に進む</Button>
+						</Grid>
 					</Grid>
 					
 					
@@ -301,6 +349,3 @@ const mapStateProps = (state) => {
 }
 
 export default connect( mapStateProps, { fetchClickedItem })(ItemDetail);
-
-
-
