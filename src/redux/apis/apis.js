f3b438fa2991d723220636e529558adafad3114a
pmg0738 <pmg0738@naver.com>
2019-12-13 07:05:16 +0900

    aaa

--- src/redux/apis/apis.js
+++ src/redux/apis/apis.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://challecara-pok-2019.lolipop.io/api/vi'
+    baseURL: 'https://challecara-pok-2019.lolipop.io/api/v1'
 })
